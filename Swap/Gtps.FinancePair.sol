d8f38492bf40fc0c5ad63d1d2179098113a3a739
GtpsFinance <welcome@gtps.finance>
2022-10-03 08:31:52 +0100

    Update Gtps.FinancePair.sol

--- Swap/Gtps.FinancePair.sol
+++ Swap/Gtps.FinancePair.sol
@@ -2,14 +2,14 @@
 
 pragma solidity =0.6.12;
 
-import './CatERC20.sol';
+import './Gtps.FinanceERC20.sol';
 import './libraries/Math.sol';
 import './libraries/UQ112x112.sol';
 import './interfaces/IERC20.sol';
 import './interfaces/ICatFactory.sol';
 import './interfaces/ICatCallee.sol';
 
-contract CatPair is CatERC20 {
+contract Gtps.FinancePair is Gtps.FinanceERC20 {
     using SafeMathUniswap  for uint;
     using UQ112x112 for uint224;
 
@@ -30,7 +30,7 @@ contract CatPair is CatERC20 {
 
     uint private unlocked = 1;
     modifier lock() {
-        require(unlocked == 1, 'PolyCat: LOCKED');
+        require(unlocked == 1, 'Gtps.Finance: LOCKED');
         unlocked = 0;
         _;
         unlocked = 1;
@@ -44,7 +44,7 @@ contract CatPair is CatERC20 {
 
     function _safeTransfer(address token, address to, uint value) private {
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(SELECTOR, to, value));
-        require(success && (data.length == 0 || abi.decode(data, (bool))), 'PolyCat: TRANSFER_FAILED');
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'Gtps.Finance: TRANSFER_FAILED');
     }
 
     event Mint(address indexed sender, uint amount0, uint amount1);
@@ -65,14 +65,14 @@ contract CatPair is CatERC20 {
 
     // called once by the factory at time of deployment
     function initialize(address _token0, address _token1) external {
-        require(msg.sender == factory, 'PolyCat: FORBIDDEN'); // sufficient check
+        require(msg.sender == factory, 'Gtps.Finance: FORBIDDEN'); // sufficient check
         token0 = _token0;
         token1 = _token1;
     }
 
     // update reserves and, on the first call per block, price accumulators
     function _update(uint balance0, uint balance1, uint112 _reserve0, uint112 _reserve1) private {
-        require(balance0 <= uint112(-1) && balance1 <= uint112(-1), 'PolyCat: OVERFLOW');
+        require(balance0 <= uint112(-1) && balance1 <= uint112(-1), 'Gtps.Finance: OVERFLOW');
         uint32 blockTimestamp = uint32(block.timestamp % 2**32);
         uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
         if (timeElapsed > 0 && _reserve0 != 0 && _reserve1 != 0) {
@@ -88,7 +88,7 @@ contract CatPair is CatERC20 {
 
     // if fee is on, mint liquidity equivalent to 14/24th of the growth in sqrt(k)
     function _mintFee(uint112 _reserve0, uint112 _reserve1) private returns (bool feeOn) {
-        address feeTo = ICatFactory(factory).feeTo();
+        address feeTo = IGtps.FinanceFactory(factory).feeTo();
         feeOn = feeTo != address(0);
         uint _kLast = kLast; // gas savings
         if (feeOn) {
@@ -123,7 +123,7 @@ contract CatPair is CatERC20 {
         } else {
             liquidity = Math.min(amount0.mul(_totalSupply) / _reserve0, amount1.mul(_totalSupply) / _reserve1);
         }
-        require(liquidity > 0, 'PolyCat: INSUFFICIENT_LIQUIDITY_MINTED');
+        require(liquidity > 0, 'Gtps.Finance: INSUFFICIENT_LIQUIDITY_MINTED');
         _mint(to, liquidity);
 
         _update(balance0, balance1, _reserve0, _reserve1);
@@ -144,7 +144,7 @@ contract CatPair is CatERC20 {
         uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
         amount0 = liquidity.mul(balance0) / _totalSupply; // using balances ensures pro-rata distribution
         amount1 = liquidity.mul(balance1) / _totalSupply; // using balances ensures pro-rata distribution
-        require(amount0 > 0 && amount1 > 0, 'PolyCat: INSUFFICIENT_LIQUIDITY_BURNED');
+        require(amount0 > 0 && amount1 > 0, 'Gtps.Finance: INSUFFICIENT_LIQUIDITY_BURNED');
         _burn(address(this), liquidity);
         _safeTransfer(_token0, to, amount0);
         _safeTransfer(_token1, to, amount1);
@@ -158,29 +158,29 @@ contract CatPair is CatERC20 {
 
     // this low-level function should be called from a contract which performs important safety checks
     function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
-        require(amount0Out > 0 || amount1Out > 0, 'PolyCat: INSUFFICIENT_OUTPUT_AMOUNT');
+        require(amount0Out > 0 || amount1Out > 0, 'Gtps.Finance: INSUFFICIENT_OUTPUT_AMOUNT');
         (uint112 _reserve0, uint112 _reserve1,) = getReserves(); // gas savings
-        require(amount0Out < _reserve0 && amount1Out < _reserve1, 'PolyCat: INSUFFICIENT_LIQUIDITY');
+        require(amount0Out < _reserve0 && amount1Out < _reserve1, 'Gtps.Finance: INSUFFICIENT_LIQUIDITY');
 
         uint balance0;
         uint balance1;
         { // scope for _token{0,1}, avoids stack too deep errors
         address _token0 = token0;
         address _token1 = token1;
-        require(to != _token0 && to != _token1, 'PolyCat: INVALID_TO');
+        require(to != _token0 && to != _token1, 'Gtps.Finance: INVALID_TO');
         if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
         if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
-        if (data.length > 0) ICatCallee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
+        if (data.length > 0) IGtps.FinanceCallee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
         balance0 = IERC20Uniswap(_token0).balanceOf(address(this));
         balance1 = IERC20Uniswap(_token1).balanceOf(address(this));
         }
         uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
         uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
-        require(amount0In > 0 || amount1In > 0, 'PolyCat: INSUFFICIENT_INPUT_AMOUNT');
+        require(amount0In > 0 || amount1In > 0, 'Gtps.Finance: INSUFFICIENT_INPUT_AMOUNT');
         { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
         uint balance0Adjusted = balance0.mul(10000).sub(amount0In.mul(24));
         uint balance1Adjusted = balance1.mul(10000).sub(amount1In.mul(24));
-        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(10000**2), 'PolyCat: K');
+        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(10000**2), 'Gtps.Finance: K');
         }
 
         _update(balance0, balance1, _reserve0, _reserve1);
