3bcdb3751b3bbf8fc6e221a8fc46168d8c688c11
GtpsFinance <welcome@gtps.finance>
2022-10-03 08:10:11 +0100

    Update IGtps.FinanceFactory.sol

--- Swap/interfaces/IGtps.FinanceFactory.sol
+++ Swap/interfaces/IGtps.FinanceFactory.sol
@@ -2,7 +2,7 @@
 
 pragma solidity >=0.5.0;
 
-interface ICatFactory {
+interface IGtps.FinanceFactory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
     function feeTo() external view returns (address);
