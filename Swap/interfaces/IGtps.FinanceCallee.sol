730f88526911cd2a31f9e9d3c85bd48715d033ab
GtpsFinance <welcome@gtps.finance>
2022-10-03 08:08:04 +0100

    Update IGtps.FinanceCallee.sol

--- Swap/interfaces/IGtps.FinanceCallee.sol
+++ Swap/interfaces/IGtps.FinanceCallee.sol
@@ -2,6 +2,6 @@
 
 pragma solidity >=0.5.0;
 
-interface ICatCallee {
+interface IGtps.FinanceCallee {
     function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external;
 }
